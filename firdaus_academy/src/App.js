2a5f7618df589e3a3d5319fd6434e7d794a3f6bb
DonJuan <armvnov1ch@gmail.com>
2024-09-23 20:16:24 +0500

    test

---
+++ firdaus_academy/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+   return (
+      <div className="full-page-background">
+         <div className="container">
+            <p>Hello world</p>
+            <p>Hello world</p>
+         </div>
+      </div>
+   );
+}
+
+export default App;
